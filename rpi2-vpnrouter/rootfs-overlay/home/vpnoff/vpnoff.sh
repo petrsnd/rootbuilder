84596a6c6dfdf4febbff4cc4c8688e575c3b408c
petrsnd <petrsnd@gmail.com>
2016-12-23 14:22:46 -0700

    vpnon and vpnoff scripts

--- rpi2-vpnrouter/rootfs-overlay/home/vpnoff/vpnoff.sh
+++ rpi2-vpnrouter/rootfs-overlay/home/vpnoff/vpnoff.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
-echo "Disabling VPN"
+if [ ! -f "/etc/ppp/peers/rpivpn" ]; then
+    echo "VPN gateway is not configured"
+    echo -e "You must configure the VPN gateway using ssh as the vpnsetup user\n"
+    read -p "Press any key..." X
+    exit 1
+fi
+
+echo -e "Disabling VPN\n"
+sudo poff
+
+echo -e "\nVerifying configuration\n"
+echo "Looking for ppp interface:"
+sudo ifconfig ppp0
+echo -e "\niptables configuration:"
+sudo iptables -L
