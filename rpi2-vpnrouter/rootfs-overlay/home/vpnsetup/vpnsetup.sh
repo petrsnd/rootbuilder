bb92e1538ebe1b40b654586684c90d8030b904da
petrsnd <petrsnd@gmail.com>
2016-12-24 15:51:35 -0700

    Adding support for remote DNS servers and routing

--- rpi2-vpnrouter/rootfs-overlay/home/vpnsetup/vpnsetup.sh
+++ rpi2-vpnrouter/rootfs-overlay/home/vpnsetup/vpnsetup.sh
@@ -33,7 +33,32 @@ Password=
 read -s -p "Password: " Password
 >&2 echo
 
-echo -e "\nOverwriting peer configuration"
+DnsServers=
+echo "List of remote DNS servers; empty string to end list"
+while : ; do
+    read -p "Remote DNS Server: " DnsServer
+    if ! [[ $DnsServer =~ ^((1?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9]{1,2}|2[0-4][0-9]|25[0-5])$ ]]; then
+        echo "Not an IP address, try again"
+        continue
+    fi
+    if [ -z "$DnsServer" ]; then
+        break
+    fi
+    DnsServers="$DnsServers$DnsServer "
+done
+
+RemoteNetwork=
+echo "The remote network is needed to set up routing, please use CIDR notation (e.g. 10.0.0.0/8)"
+while [ -z "$RemoteNetwork" ]; do
+    read -p "VPN address: " VpnAddress
+    if ! [[ $RemoteNetwork =~ ^((1?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9]{1,2}|2[0-4][0-9]|25[0-5])/[0-9]{1,2}$ ]]; then
+        echo "Not CIDR notation, please use a valid network such as 10.0.0.0/8"
+        RemoteNetwork=
+    fi
+done
+
+
+echo -e "\nWriting peer configuration"
 sudo cat <<EOF | sudo tee /etc/ppp/peers/rpivpn > /dev/null
 pty "pptp $VpnAddress --nolaunchpppd"
 name $(echo $UserName | sed -e 's,\\,\\\\,')
@@ -47,3 +72,38 @@ EOF
 echo "Writing the credential file"
 sudo bash -c "sed -i -e '/PPTP/d' /etc/ppp/chap-secrets"
 sudo echo -e "$(echo $UserName | sed -e 's,\\,\\\\,') PPTP $Password *" | sudo tee -a /etc/ppp/chap-secrets > /dev/null
+
+echo "Writing the resolver configuration"
+sudo cat <<EOF | sudo tee /etc/ppp/resolv.conf > /dev/null
+$(for s in $DnsServers; do echo -e "nameserver $s"; done)
+EOF
+
+echo "Writing routing scripts"
+sudo cat <<EOF | sudo tee /etc/ppp/ip-up.d/000routing > /dev/null
+#!/bin/sh
+
+# Randomly added route??
+route del 166.70.162.27
+
+# VPN remote network route
+route add -net $RemoteNetwork dev ppp0
+
+# Forwarding for other computers on my LAN
+iptables --insert OUTPUT 1 --source 0.0.0.0/0.0.0.0 --destination $RemoteNetwork --jump ACCEPT --out-interface ppp0
+iptables --insert INPUT 1 --source $RemoteNetwork --destination 0.0.0.0/0.0.0.0 --jump ACCEPT --in-interface ppp0
+iptables --insert FORWARD 1 --source 0.0.0.0/0.0.0.0 --destination $RemoteNetwork --jump ACCEPT --out-interface ppp0
+iptables --insert FORWARD 1 --source $RemoteNetwork --destination 0.0.0.0/0.0.0.0 --jump ACCEPT
+iptables --table nat --append POSTROUTING --out-interface ppp0 --jump MASQUERADE
+iptables --append FORWARD --protocol tcp --tcp-flags SYN,RST SYN --jump TCPMSS --clamp-mss-to-pmtu
+EOF
+sudo cat <<EOF | sudo tee /etc/ppp/ip-down.d/000routing > /dev/null
+#!/bin/sh
+
+# VPN remote network route
+route del -net $RemoteNetwork
+
+# Remove forwarding for LAN
+iptables -F
+iptables -t nat -F
+EOF
+
