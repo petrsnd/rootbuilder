f3bb5d0844d4cba472f98324c4abb225e3d14cc0
petrsnd <petrsnd@gmail.com>
2016-12-31 23:36:55 -0700

    Forgot to let command hit before killing screen

--- rpi2-vpnrouter/rootfs-overlay/home/vpnoff/vpnoff.sh
+++ rpi2-vpnrouter/rootfs-overlay/home/vpnoff/vpnoff.sh
@@ -10,7 +10,8 @@ fi
 sudo screen -list | grep -q "rpivpn"
 if [ $? -eq 0 ]; then 
     echo -e "Disabling VPN\n"
-    sudo screen -r "rpivpn" -X stuff $'sudo poff\n'
+    sudo screen -r "rpivpn" -X stuff $'sudo poff rpivpn\n'
+    sleep 3
 else
     echo -e "VPN does not appear to be running\n"
 fi
@@ -19,7 +20,7 @@ echo -e "Tearing down screen session\n"
 sudo screen -r "rpivpn" -p 0 -X quit
 
 echo -e "Sleeping for 3 seconds...\n"
-sleep 5
+sleep 3
 
 echo -e "\nVerifying configuration\n"
 echo "Looking for ppp interface:"
