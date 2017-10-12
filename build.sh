dc45058a18756033bc7c1e3763864054555ac9e5
petrsnd <petrsnd@gmail.com>
2017-10-12 13:43:35 -0600

    convenience scripts

--- build.sh
+++ build.sh
@@ -3,7 +3,16 @@
 ScriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ ! -z "$(which docker)" ]; then
-    docker build -t rootbuilder $ScriptDir
+    if [ -z "$(docker images -q rootbuilder)" ]; then
+        docker build -t rootbuilder $ScriptDir
+    else
+        if [ "$1" = "-f" ]; then
+            docker rmi rootbuilder
+            docker build -t rootbuilder $ScriptDir
+        else
+            >&2 echo "The rootbuilder image already exists, use -f flag to rebuild"
+        fi
+    fi
 else
     >&2 echo "You must install docker to use this build script"
 fi
